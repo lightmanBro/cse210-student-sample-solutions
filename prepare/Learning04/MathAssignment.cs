e3fe8fc0c9d54d7b8561a3fcf4f8465f70b347d2
Scott Burton <burtons@byui.edu>
2022-12-09 15:54:52 -0700

    Learning activity 05

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -3,7 +3,7 @@ public class MathAssignment : Assignment
     private string _textbookSection;
     private string _problems;
 
-    // Notice the syntax here that the Math assignment constructor has 4 parameters and then
+    // Notice the syntax here that the MathAssignment constructor has 4 parameters and then
     // it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor.
     public MathAssignment(string studentName, string topic, string textbookSection, string problems)
         : base(studentName, topic)
